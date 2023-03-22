a132abb115c4e0340c0c6b5c4db34c8ff48cc56f
Nalin <nalinbhardwaj@nibnalin.me>
2023-03-23 01:18:21 +0700

    Bump version and publish to crates.io (#9)

--- src/lib.rs
+++ src/lib.rs
@@ -28,11 +28,13 @@ pub mod circom;
 pub type G1 = pasta_curves::pallas::Point;
 pub type F1 = <G1 as Group>::Scalar;
 pub type EE1 = nova_snark::provider::ipa_pc::EvaluationEngine<G1>;
-pub type S1 = nova_snark::spartan::RelaxedR1CSSNARK<G1, EE1>;
+pub type CC1 = nova_snark::spartan::spark::TrivialCompComputationEngine<G1, EE1>;
+pub type S1 = nova_snark::spartan::RelaxedR1CSSNARK<G1, EE1, CC1>;
 pub type G2 = pasta_curves::vesta::Point;
 pub type F2 = <G2 as Group>::Scalar;
 pub type EE2 = nova_snark::provider::ipa_pc::EvaluationEngine<G2>;
-pub type S2 = nova_snark::spartan::RelaxedR1CSSNARK<G2, EE2>;
+pub type CC2 = nova_snark::spartan::spark::TrivialCompComputationEngine<G2, EE2>;
+pub type S2 = nova_snark::spartan::RelaxedR1CSSNARK<G2, EE2, CC2>;
 
 type C1 = CircomCircuit<<G1 as Group>::Scalar>;
 type C2 = TrivialTestCircuit<<G2 as Group>::Scalar>;
@@ -128,7 +130,7 @@ pub fn create_recursive_circuit(
             r1cs: r1cs.clone(),
             witness: Some(witness),
         };
-        
+
         let current_public_output = circuit.get_public_outputs();
         current_public_input = current_public_output
             .iter()
